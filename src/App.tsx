606bc902563a95ba240b7652cd1904adcfda114b
Lee <leesjensen@gmail.com>
2023-11-04 04:36:59 -0600

    impl(website) content #4

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,7 @@ function App() {
       <h1>Startup 360!</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Classic website design</p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
